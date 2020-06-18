ecfe64774619e677e4bba7b8afc62976922d3822
Shahbaz5523 <shahbaz.rajpoot15@gmail.com>
2020-06-18 15:06:23 +0500

    Preloader in web-page

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load',()=>{
+
+    let loader = document.getElementById("loader");
+    loader.classList.add("hidden");
+
+});
+
+// Thanks 4 watching my video like it and share it with others if you really like my work and also 
+// subscribe my channal and press the bell icon
